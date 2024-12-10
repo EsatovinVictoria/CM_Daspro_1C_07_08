9093f46a013c262cf4dab42344b05c507da25fe4
Fadillah-Dama <damarfky@gmail.com>
2024-12-10 12:19:43 +0700

    dama 1

--- src/App.java
+++ src/App.java
@@ -1,4 +1,9 @@
 public class App {
+
+    static void displayData() {
+        System.out.println("=== DATA INVENTORI BARANG ===");
+    }
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
